6fd2ecfdac5b3123e66477bc32fe0bdf7eac77f3
Michael Gale <michael@michaelgale.dev>
2023-01-27 08:46:14 +1100

    feat: add complete track data

--- src/pages/every-alterations/[...slug].vue
+++ src/pages/every-alterations/[...slug].vue
@@ -1,11 +1,9 @@
 <template>
   <c-section>
-    <!-- <h1 v-if="title">Track {{ id }}: <em>{{ title }}</em></h1> -->
+    <h1 v-if="title">Track {{ id }}: <em>{{ title }}</em></h1>
     <!-- <nuxt-img v-if="image" :src="image" /> -->
-    <content-doc :path="`/story/${slug}`"/>
-    <!-- <audio v-if="audio" src="audio" controls playsinline="" loop autoplay></audio> -->
-    <!-- <atoms-spotify-embed v-if="embed" :src="embed" /> -->
-    <!-- <molecules-chapter-navigation /> -->
+    <content-doc :path="`/tracks/${slug}`"/>
+    <atoms-spotify-embed v-if="embed" :src="embed" />
   </c-section>
 </template>
 
@@ -14,16 +12,11 @@ const route = useRoute()
 // Get the chapter ID from the route
 const [slug] = route.params.slug
 
-const { data } = await useAsyncData('tracks-data', () => queryContent('/tracks-data').findOne())
+// Get the track data
+const { data:track } = await useAsyncData('track', () => {
+  return queryContent(`/tracks/${slug}`).findOne()
+})
 
-// const { data } = await useAsyncData('tracks', () => {
-//   return queryContent().where({ _path: path }).only(['title']).findOne()
-// })
-
-console.log(data)
-
-// const tracks = story.value.tracks
-
-// pluck the chapter info from the story
-// const { id, title, image, audio, embed } = methods.getTrackBySlug(tracks, slug)
-</script> 
+// Pull out the track title and ID
+const { id, title, embed } = track.value
+</script>
