25f11f5bf9446dd2606b6f25ae8e107f891ec8da
Michael Gale <michael@michaelgale.dev>
2023-01-28 16:30:08 +1100

    fix: chapter list

--- src/components/atoms/chapter-list/chapter-list.vue
+++ src/components/atoms/chapter-list/chapter-list.vue
@@ -1,20 +1,13 @@
-<script setup>
-const tracks = []
-</script>
-
 <template>
- <aside>
-    <details>
-      <summary>Chapter select</summary>
-      <nav for="select-chapter">
-        <ol id="select-chapter" role="list">
-          <li v-for="chapter in tracks" role="listitem">
-            <nuxt-link :to="`/every-alterations/${chapter.slug}`">
-              {{ chapter.id }}. {{ chapter.title }}
-            </nuxt-link>
-          </li>
-        </ol>
-      </nav>
-    </details>
+  <aside>
+    <ContentList path="/tracks" v-slot="{ list }">
+      <div v-for="track in list" :key="track._path">
+        <h2>{{ track.title }}</h2>
+        <p>{{ track.description.includes('Instrumental') ? ':)' : track.description }}</p>
+        <nuxt-link :to="`/every-alterations/${track.slug}`">
+          Learn more about <em>{{ track.title }}</em>
+        </nuxt-link>
+      </div>
+    </ContentList>
   </aside>
 </template>
